725c6c2d8caf52f4784c3a197909c7a23d5b7649
Hugo Dor <hugo.dor@student.kdg.be>
2025-11-14 13:11:27 +0100

    lege packages structuur

--- src/main/java/be/kdg/ip3/archportal/ArchPortalApplication.java
+++ src/main/java/be/kdg/ip3/archportal/ArchPortalApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ArchPortalApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ArchPortalApplication.class, args);
     }
 
