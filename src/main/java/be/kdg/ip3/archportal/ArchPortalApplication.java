76cfb0dc75572fc9b3e24e067aa185de24baa779
Hugo Dor <hugo.dor@student.kdg.be>
2025-11-14 12:50:36 +0100

    Initial commit

---
+++ src/main/java/be/kdg/ip3/archportal/ArchPortalApplication.java
@@ -0,0 +1,13 @@
+package be.kdg.ip3.archportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/kdg/ip3/archportal/ArchPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package be.kdg.ip3.archportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
