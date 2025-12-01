fa171f753976aa1bfcc00d783bc78c10dde51306
hugo.dor <hugo.dor@student.kdg.be>
2025-12-01 16:20:46 +0100

    Mail Service toegevoegd samen met instelingen voor smpt connectie met mail, en secret aangemaakt voor wachtwoorden enzo bruh

--- src/main/java/be/kdg/ip3/archportal/communications/infrastructure/notification/DbNotificationRepository.java
+++ src/main/java/be/kdg/ip3/archportal/communications/infrastructure/notification/DbNotificationRepository.java
@@ -1,10 +1,11 @@
 package be.kdg.ip3.archportal.communications.infrastructure.notification;
 
+import be.kdg.ip3.archportal.communications.domain.notification.NotificationRepository;
 import be.kdg.ip3.archportal.communications.infrastructure.notification.jpa.JpaNotificationRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class DbNotificationRepository {
+public class DbNotificationRepository implements NotificationRepository {
     private final JpaNotificationRepository repository;
     public DbNotificationRepository(JpaNotificationRepository repository) {
         this.repository = repository;
