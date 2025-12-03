94d1696c19b18f7ad8d0efd3bbda06f76750ce9c
hugo.dor <hugo.dor@student.kdg.be>
2025-12-03 15:37:07 +0100

    IO PRINT FIX

--- src/main/java/be/kdg/ip3/archportal/communications/domain/settings/NotificationSettings.java
+++ src/main/java/be/kdg/ip3/archportal/communications/domain/settings/NotificationSettings.java
@@ -4,7 +4,6 @@ import be.kdg.ip3.archportal.communications.domain.notification.ChannelType;
 import lombok.Getter;
 import org.jmolecules.ddd.annotation.Entity;
 
-import java.util.Collections;
 import java.util.List;
 
 
@@ -27,7 +26,6 @@ public class NotificationSettings {
         if (this.channelType.contains(channelType)) {
             throw new IllegalArgumentException("Cant add the same channel type to preferences twice!");
         }
-        IO.println(channelType);
         this.channelType.add(channelType);
     }
 
@@ -35,7 +33,6 @@ public class NotificationSettings {
         if (!this.channelType.contains(channelType)) {
             throw new IllegalArgumentException("Cant remove a channel type that is not in your preferences!");
         }
-        IO.println(channelType);
         this.channelType.remove(channelType);
     }
 }
