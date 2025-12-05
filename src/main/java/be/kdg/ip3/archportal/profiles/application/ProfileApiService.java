31aaa233691d89b73135443313214075a31b5741
axel peeter <axel.peeters.1@student.kdg.be>
2025-12-05 19:42:45 +0100

    mr conflicts opgelost

--- src/main/java/be/kdg/ip3/archportal/profiles/application/ProfileApiService.java
+++ src/main/java/be/kdg/ip3/archportal/profiles/application/ProfileApiService.java
@@ -1,5 +1,7 @@
 package be.kdg.ip3.archportal.profiles.application;
 
+import be.kdg.ip3.archportal.analytics.shared.AnalyticsApi;
+import be.kdg.ip3.archportal.analytics.shared.CreateGameStatsDto;
 import be.kdg.ip3.archportal.games.shared.GamesApi;
 import be.kdg.ip3.archportal.profiles.domain.profile.ProfileId;
 import be.kdg.ip3.archportal.profiles.domain.profile.ProfileRepository;
@@ -15,10 +17,12 @@ import java.util.UUID;
 public class ProfileApiService implements ProfilesApi {
     private final ProfileRepository profileRepository;
     private final GamesApi gamesApi;
+    private final AnalyticsApi analyticsApi;
 
-    public ProfileApiService(ProfileRepository profileRepository, GamesApi gamesApi) {
+    public ProfileApiService(ProfileRepository profileRepository, GamesApi gamesApi, AnalyticsApi analyticsApi) {
         this.profileRepository = profileRepository;
         this.gamesApi = gamesApi;
+        this.analyticsApi = analyticsApi;
     }
 
     @Override
@@ -71,5 +75,10 @@ public class ProfileApiService implements ProfilesApi {
 
         games.forEach(profile::acquireGame);
         profileRepository.save(profile);
+
+        for (var game: games){
+            analyticsApi.instantiateGameStatistics(new CreateGameStatsDto(game, profileId));
+        }
+
     }
 }
