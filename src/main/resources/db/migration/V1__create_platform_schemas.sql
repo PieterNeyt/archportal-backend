7f90ef616aa17f2ceef4cd06018253a92a02f437
hugo.dor <hugo.dor@student.kdg.be>
2025-12-16 16:17:00 +0100

    changes voor flyway

---
+++ src/main/resources/db/migration/V1__create_platform_schemas.sql
@@ -0,0 +1,6 @@
+create schema if not exists gameservice;
+create schema if not exists analyticsservice;
+create schema if not exists lobbyservice;
+create schema if not exists shopservice;
+create schema if not exists communicationservice;
+create schema if not exists profileservice;
