4f942a09088c9757d020f077f60a4c46d92c50e1
Cian Van Acker <cian.vanacker@student.kdg.be>
2025-12-20 19:08:25 +0100

    correctere validatie CreatePartyDto

--- src/main/java/be/kdg/ip3/archportal/lobbies/api/dto/CreatePartyDto.java
+++ src/main/java/be/kdg/ip3/archportal/lobbies/api/dto/CreatePartyDto.java
@@ -2,7 +2,8 @@ package be.kdg.ip3.archportal.lobbies.api.dto;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Length;
 
-public record CreatePartyDto(@NotBlank String title, @Min(1) @Max(10) int maxMembers) {
+public record CreatePartyDto(@NotEmpty @Length(max = 100) String title, @Min(2) @Max(10) int maxMembers) {
 }
