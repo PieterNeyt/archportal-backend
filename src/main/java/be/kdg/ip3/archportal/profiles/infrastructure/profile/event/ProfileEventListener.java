62c1316e464f7c3dc51002bfb32db19d00e4c878
Pieter Neyt <pieter.neyt@student.kdg.be>
2025-12-23 15:20:26 +0100

    event nu in infrastructure gezet

---
+++ src/main/java/be/kdg/ip3/archportal/profiles/infrastructure/profile/event/ProfileEventListener.java
@@ -0,0 +1,25 @@
+package be.kdg.ip3.archportal.profiles.infrastructure.profile.event;
+
+import be.kdg.ip3.archportal.profiles.application.ProfileService;
+import be.kdg.ip3.archportal.profiles.application.command.AcquirePlatformPointsCommand;
+import be.kdg.ip3.archportal.profiles.shared.GrantPlatformPointsEvent;
+import org.springframework.modulith.events.ApplicationModuleListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProfileEventListener {
+
+    private final ProfileService profileService;
+    public ProfileEventListener( ProfileService profileService) {
+        this.profileService = profileService;
+    }
+
+    @Async
+    @ApplicationModuleListener
+    public void onGrantPlatformPoints(GrantPlatformPointsEvent event) {
+        profileService.aquirePlatformPoints(
+                AcquirePlatformPointsCommand.fromDto(event)
+        );
+    }
+}
