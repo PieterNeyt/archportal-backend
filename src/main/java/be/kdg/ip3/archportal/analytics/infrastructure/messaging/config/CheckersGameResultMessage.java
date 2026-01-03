1f11d0549aec0f06dca4d70988a94a3f7bbd21f6
Pieter Neyt <pieter.neyt@student.kdg.be>
2026-01-03 01:51:40 +0100

    achievement unlock setup

---
+++ src/main/java/be/kdg/ip3/archportal/analytics/infrastructure/messaging/config/CheckersGameResultMessage.java
@@ -0,0 +1,7 @@
+package be.kdg.ip3.archportal.analytics.infrastructure.messaging.config;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record CheckersGameResultMessage(UUID sessionId, String winner, LocalDateTime timestamp) {
+}
--- src/main/java/be/kdg/ip3/archportal/analytics/infrastructure/messaging/config/RabbitMQTopology.java
+++ src/main/java/be/kdg/ip3/archportal/analytics/infrastructure/messaging/config/RabbitMQTopology.java
@@ -12,7 +12,11 @@ public class RabbitMQTopology {
     public static final String TTT_EXCHANGE_NAME = "ttt-exchange";
     public static final String TTT_QUEUE_NAME = "ttt-queue";
 
+    public static final String CHECKERS_EXCHANGE_NAME = "checkers-exchange";
+    public static final String CHECKERS_QUEUE_NAME = "checkers-queue";
 
+    public static final String ACHIEVEMENT_EXCHANGE_NAME = "achievement-exchange";
+    public static final String ACHIEVEMENT_QUEUE_NAME = "achievement-queue";
     // ttt topology
 
     @Bean
@@ -28,5 +32,36 @@ public class RabbitMQTopology {
     @Bean
     Binding tttQueueToTttExchangeBinding() {
         return BindingBuilder.bind(tttQueue()).to(tttExchange()).with("ttt.game.*");
+    }
+
+    // Checkers
+    @Bean
+    TopicExchange checkersExchange() {
+        return new TopicExchange(CHECKERS_EXCHANGE_NAME);
+    }
+
+    @Bean
+    Queue checkersQueue() {
+        return QueueBuilder.nonDurable(CHECKERS_QUEUE_NAME).build();
+    }
+
+    @Bean
+    Binding checkersQueueToCheckersExchangeBinding() {
+        return BindingBuilder.bind(checkersQueue()).to(checkersExchange()).with("checkers.game.*");
+    }
+    // Achievement topology
+    @Bean
+    TopicExchange achievementExchange() {
+        return new TopicExchange(ACHIEVEMENT_EXCHANGE_NAME);
+    }
+
+    @Bean
+    Queue achievementQueue() {
+        return QueueBuilder.nonDurable(ACHIEVEMENT_QUEUE_NAME).build();
+    }
+
+    @Bean
+    Binding achievementQueueToAchievementExchangeBinding() {
+        return BindingBuilder.bind(achievementQueue()).to(achievementExchange()).with("*.achievement.unlock");
     }
 }
